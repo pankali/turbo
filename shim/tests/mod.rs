864cb7f7fbddb6133eb49ca454591023d73c1887
LongYinan <lynweklm@gmail.com>
2022-10-27 22:47:15 +0800

    Update Cargo dependencies, add shim into Cargo workspace

--- shim/tests/mod.rs
+++ shim/tests/mod.rs
@@ -1,7 +1,7 @@
 use assert_cmd::Command;
 
 #[test]
-fn test_find_correct_turbo() {
+fn test_no_arguments() {
     let mut cmd = Command::cargo_bin("turbo").unwrap();
     cmd.assert()
         .append_context("turbo", "no arguments")
@@ -10,9 +10,12 @@ fn test_find_correct_turbo() {
             "`turbo` with no arguments should exit with code 1",
         )
         .code(1);
+}
 
+#[test]
+fn test_find_turbo_in_example() {
     let mut cmd = Command::cargo_bin("turbo").unwrap();
-    cmd.args(&["--cwd", "../examples/basic", "bin"])
+    cmd.args(["--cwd", "../examples/basic", "bin"])
         .assert()
         .append_context(
             "turbo",
@@ -27,9 +30,12 @@ fn test_find_correct_turbo() {
         .stdout(predicates::str::ends_with(
             "examples/basic/node_modules/.bin/turbo\n",
         ));
+}
 
+#[test]
+fn test_find_correct_turbo() {
     let mut cmd = Command::cargo_bin("turbo").unwrap();
-    cmd.args(&["--cwd", "..", "bin"])
+    cmd.args(["--cwd", "..", "bin"])
         .assert()
         .append_context(
             "turbo",
